26533e59de868af88259591258b6a25c33bcce53
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-05-04 22:32:19 +0300

    Solution

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from cinema.models import Movie
+from cinema.models import Movie, Actor, Genre, CinemaHall
 
 
 @admin.register(Movie)
 class AdminMovie(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Actor)
+class AdminActor(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class AdminGenre(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CinemaHall)
+class AdminCinemaHall(admin.ModelAdmin):
     pass
