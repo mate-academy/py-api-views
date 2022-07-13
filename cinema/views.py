cbe96af4c1684da4a1be8a58cdce9aa48a4e139d
Elena <lenasharabura@gmail.com>
2022-07-13 21:00:10 +0200

    change GenreDetailView patch

--- cinema/views.py
+++ cinema/views.py
@@ -48,10 +48,11 @@ class GenreDetail(APIView):
 
     def patch(self, request, pk):
         genre = get_object_or_404(Genre, pk=pk)
-        serializer = GenreSerializer(genre, data=request.data)
+        serializer = GenreSerializer(genre, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         genre = get_object_or_404(Genre, pk=pk)
