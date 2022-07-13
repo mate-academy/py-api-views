ddc0761e0f48385b2ec34f53f59668228dd25610
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-07-13 20:48:47 +0300

    Fix imports in cinema.url

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from cinema.views import GenreList, GenreDetail, ActorListView, \
-    ActorDetailView, CinemaHallListView, \
-    CinemaHallDetailView, MovieViewSet
+from cinema.views import GenreList, GenreDetail, ActorList, \
+    ActorDetail, CinemaHallList, \
+    CinemaHallDetail, MovieViewSet
 
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
@@ -12,11 +12,11 @@ urlpatterns = [
     path("", include(router.urls)),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("actors/", ActorListView.as_view(), name="actor-list"),
-    path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
-    path("cinema-halls/", CinemaHallListView.as_view(),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema-halls/", CinemaHallList.as_view(),
          name="cinema-hall-list"),
-    path("cinema-halls/<int:pk>/", CinemaHallDetailView.as_view(),
+    path("cinema-halls/<int:pk>/", CinemaHallDetail.as_view(),
          name="cinema-hall-detail"),
 
 ]
