32e21bbe850099c78b0adf0a69a467a2e0cbe5dd
Taras Nester <tarasnester414@gmail.com>
2022-07-13 16:45:04 +0300

    fixed post method

--- cinema/views.py
+++ cinema/views.py
@@ -25,6 +25,8 @@ class GenreList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class GenreDetail(APIView):
     def get(self, request, pk):
