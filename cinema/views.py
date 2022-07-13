5cd486efa43f08b0d8d4d821dc804e3a59cf98af
Flashmobber <ufinaid@gmail.com>
2022-07-13 17:11:39 +0300

    HTTP_400_BAD_REQUEST added

--- cinema/views.py
+++ cinema/views.py
@@ -26,6 +26,8 @@ class GenreList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class GenreDetail(APIView):
     @staticmethod
