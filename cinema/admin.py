10d29b9423c4f404298e8a38abad98a29bae91d1
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-07-13 20:33:27 +0300

    Solution

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from cinema.models import Movie
+from cinema.models import Movie, Genre, Actor, CinemaHall
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ("title",)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    search_fields = ("first_name", "last_name",)
+
+
+@admin.register(CinemaHall)
+class CinemaHall(admin.ModelAdmin):
+    search_fields = ("nama",)
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,24 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import GenreList, GenreDetail, ActorListView, \
+    ActorDetailView, CinemaHallListView, \
+    CinemaHallDetailView, MovieViewSet
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls)),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorListView.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
+    path("cinema-halls/", CinemaHallListView.as_view(),
+         name="cinema-hall-list"),
+    path("cinema-halls/<int:pk>/", CinemaHallDetailView.as_view(),
+         name="cinema-hall-detail"),
+
 ]
 
 app_name = "cinema"
