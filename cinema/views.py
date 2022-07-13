88e84fcc1528bed96abb26e36b35e6ce439ff502
Evgeniy Tretiak <genoktretiack@gmail.com>
2022-07-13 22:23:19 +0300

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -14,22 +14,6 @@ from cinema.serializers import (
 )
 
 
-@api_view(["GET", "POST"])
-def movie_list(request):
-    if request.method == "GET":
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    if request.method == "POST":
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-
 class GenreList(APIView):
     def get(self, request):
         genres = Genre.objects.all()
