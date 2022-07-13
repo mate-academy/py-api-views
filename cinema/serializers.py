064f774f78f59664e2c759766f6fb90f6b188ed2
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-07-13 21:34:41 +0300

    All test are run success

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -69,7 +69,8 @@ class CinemaHallSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
         instance.rows = validated_data.get("rows", instance.rows)
-        instance.seats_in_row = validated_data.get("row", instance.rows)
+        instance.seats_in_row = validated_data.get("seats_in_row",
+                                                   instance.seats_in_row)
 
         instance.save()
 
--- cinema/urls.py
+++ cinema/urls.py
@@ -11,7 +11,7 @@ cinema_hall_detail = CinemaHallViewSet.as_view(
     actions={
         "get": "retrieve",
         "put": "update",
-        "path": "partial_update",
+        "patch": "partial_update",
         "delete": "destroy",
 
     }
@@ -26,9 +26,9 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema-halls/<int:pk>/", cinema_hall_detail,
-         name="cinema-hall-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema_hall-list"),
+    path("cinema_halls/<int:pk>/", cinema_hall_detail,
+         name="cinema_hall-detail"),
 
 ]
 
