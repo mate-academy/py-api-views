bbbc41f8e9d592d3027f6f63b1bfadcadf45bc73
Elena <lenasharabura@gmail.com>
2022-07-13 14:01:16 +0200

    rename views

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -59,7 +59,7 @@ class ActorSerializer(serializers.Serializer):
 class CinemaHallSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255, required=True)
-    row = serializers.IntegerField(required=True)
+    rows = serializers.IntegerField(required=True)
     seats_in_row = serializers.IntegerField(required=True)
 
     def create(self, validated_data):
@@ -67,7 +67,7 @@ class CinemaHallSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
-        instance.row = validated_data.get("row", instance.row)
+        instance.rows = validated_data.get("rows", instance.rows)
         instance.seats_in_row = validated_data.get("seats_in_row", instance.seats_in_row)
 
         instance.save()
