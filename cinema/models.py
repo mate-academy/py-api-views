af2def2f6bcc54c0a50eacbe3f8ba528373e7137
Yurii Kindrat <yurkinkek@gmail.com>
2022-07-14 17:57:31 +0300

    fixed the movie model

--- cinema/models.py
+++ cinema/models.py
@@ -6,7 +6,7 @@ class Movie(models.Model):
     description = models.TextField()
     duration = models.IntegerField()
     actors = models.ManyToManyField("Actor", related_name="movies")
-    genres = models.ForeignKey("Genre", on_delete=models.CASCADE, related_name="movies", null=True)
+    genres = models.ManyToManyField("Genre", related_name="movies")
 
     def __str__(self):
         return self.title
