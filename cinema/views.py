84029ba2b695cbdbe70a36ad6dfd6593821219b0
Anton Shpachuk <www.shpachyk98@gmail.com>
2022-08-08 19:43:54 +0300

    deleted imports

--- cinema/views.py
+++ cinema/views.py
@@ -1,8 +1,5 @@
-from django.http import Http404
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, generics, mixins, viewsets
-from django.shortcuts import get_object_or_404
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
