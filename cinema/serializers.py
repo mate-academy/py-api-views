af0ea02d57fa9e9dcf59c348e7d17f93593e8206
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2022-10-10 17:49:43 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,13 +1,66 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import (Actor,
+                           Genre,
+                           CinemaHall,
+                           Movie)
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255, required=True)
-    description = serializers.CharField(required=True)
-    duration = serializers.IntegerField(required=True)
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.\
+            get("first_name", instance.first_name)
+        instance.last_name = validated_data.\
+            get("last_name", instance.last_name)
+        instance.save()
+
+        return instance
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Genre.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.save()
+
+        return instance
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return CinemaHall.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.seats_in_row = validated_data.\
+            get("seats_in_row", instance.seats_in_row)
+        instance.save()
+
+        return instance
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ["title", "description", "duration"]
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
