975b4ba739ec1282581c8d35c5c391c2b5a8ba64
Oleksii Bodnarchuk <olbody12@gmail.com>
2022-10-19 13:26:06 +0300

    for mentor review, 'genre pass tests, not work in web, Cinemahall mirror case'

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,24 +1,93 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Actor, Genre, CinemaHall
 
 
-class MovieSerializer(serializers.Serializer):
+class GenreSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255, required=True)
-    description = serializers.CharField(required=True)
-    duration = serializers.IntegerField(required=True)
+    name = serializers.CharField(max_length=63, required=True, allow_blank=False)
+
+    class Meta:
+        model = Genre
+        fields = '__all__'
 
     def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+        return Genre.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get(
-            "description", instance.description
-        )
-        instance.duration = validated_data.get("duration", instance.duration)
+        instance.name = validated_data.get("name", instance.name)
 
         instance.save()
 
         return instance
+
+
+class CinemaHallSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=63, required=True)
+    rows = serializers.IntegerField(required=True)
+    seats_in_row = serializers.IntegerField(required=True)
+
+    class Meta:
+        model = CinemaHall
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return CinemaHall.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row", instance.seats_in_row)
+
+        instance.save()
+
+        return instance
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=63)
+    last_name = serializers.CharField(max_length=255)
+
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+
+        instance.save()
+
+        return instance
+
+
+class MovieActorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Actor
+        fields = ["first_name", "last_name"]
+
+
+class MovieGenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ["name"]
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    actors = MovieActorSerializer(many=True, required=False, partial=True)
+    genres = MovieGenreSerializer(many=True, required=False, partial=True)
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+
+
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,38 @@
-from django.urls import path
+from django.urls import path, include
 
-from cinema.views import movie_list, movie_detail
+from rest_framework import routers
+
+from cinema.views import (
+    GenreList,
+    GenreDetail,
+    MovieViewSet,
+    CinemaHallViewSet,
+    ActorList,
+    ActorDetail
+)
+
+movie_router = routers.DefaultRouter()
+movie_router.register("", MovieViewSet)
+
+cinema_hall_list = CinemaHallViewSet.as_view(
+        actions={"get": "list",
+                 "post": "create"})
+cinema_hall_detail = CinemaHallViewSet.as_view(
+        {
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+            "delete": "destroy",
+        })
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("movies/", include(movie_router.urls)),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_hall/", cinema_hall_list, name="cinema_hall-list"),
+    path("cinema_hall/<int:pk>/", cinema_hall_detail, name="cinema_hall-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
 ]
 
 app_name = "cinema"
