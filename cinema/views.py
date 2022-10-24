36b7380ab3588bc47ae34e56aa9e7118027b1bd3
Sergii Kantor <sergiy.kantor@gmail.com>
2022-10-24 19:32:04 +0300

    fixed remark

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,4 @@
-from django.http import Http404
+from rest_framework.generics import get_object_or_404
 
 from rest_framework.response import Response
 from rest_framework import status, mixins, generics, viewsets
@@ -31,10 +31,7 @@ class GenreList(APIView):
 
 class GenreDetail(APIView):
     def get_object(self, pk):
-        try:
-            return Genre.objects.get(pk=pk)
-        except Genre.DoesNotExist:
-            raise Http404
+        return get_object_or_404(Genre, pk=pk)
 
     def get(self, request, pk):
         genre = self.get_object(pk)
@@ -57,7 +54,7 @@ class GenreDetail(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
