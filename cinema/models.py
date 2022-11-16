20f98627cfb8e34078ebd5236d70548877365a38
igor kosianenko <funkcionalkos@gmail.com>
2022-11-16 13:47:40 +0200

    Solution)

--- cinema/models.py
+++ cinema/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=65)
+    last_name = models.CharField(max_length=65)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=65, unique=True)
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=65)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="actors")
+    genres = models.ManyToManyField(Genre, related_name="genres")
     duration = models.IntegerField()
 
     def __str__(self):
