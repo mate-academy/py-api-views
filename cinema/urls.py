6bdea9961e01ca2421798039bd2c3de831f8e499
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-16 16:17:52 +0200

    Create router for MovieViewSet

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
-from cinema.views import GenreList, GenreDetail, ActorList, ActorDetail, CinemaHallViewSet
+from cinema.views import GenreList, GenreDetail, ActorList, ActorDetail, CinemaHallViewSet, MovieViewSet
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
 
 cinema_hall_list = CinemaHallViewSet.as_view(actions={"get": "list", "post": "create"})
 cinema_hall_detail = CinemaHallViewSet.as_view(actions={
@@ -40,6 +45,10 @@ urlpatterns = [
         "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-hall-detail"
+    ),
+    path(
+        "",
+        include(router.urls)
     )
 ]
 
