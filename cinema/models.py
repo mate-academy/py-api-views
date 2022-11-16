c5852c62e9c4171b2b9ab466c0156d75853d2199
Yurii <pazyn404@gmail.com>
2022-11-17 01:30:15 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -5,6 +5,23 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    genres = models.ManyToManyField(to="Genre", related_name="movies")
+    actors = models.ManyToManyField(to="Actor", related_name="movies")
 
     def __str__(self):
         return self.title
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=63)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,47 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import path, include
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    MovieViewSet,
+    ActorList,
+    ActorDetail,
+    GenreList,
+    GenreDetail,
+    CinemaHallViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+
+cinema_hall_list = CinemaHallViewSet.as_view(
+    actions={
+        "post": "create",
+        "get": "list",
+    }
+)
+
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "patch": "partial_update",
+        "put": "update",
+        "delete": "destroy",
+    }
+)
+
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls), name="movie"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail",
+    ),
 ]
 
 app_name = "cinema"
