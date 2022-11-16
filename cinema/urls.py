726c4b9ceab9ccfd6f0c1768fdad15506b37cb90
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-16 16:15:02 +0200

    created CinemaHallViewSet using GenericViewSet

--- cinema/urls.py
+++ cinema/urls.py
@@ -6,7 +6,21 @@ from cinema.views import (
     GenreList,
     GenreDetail,
     ActorList,
-    ActorDetail
+    ActorDetail,
+    CinemaHallViewSet
+)
+
+cinema_hall_list = CinemaHallViewSet.as_view(
+    actions={"get": "list", "post": "create"}
+)
+
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
 )
 
 urlpatterns = [
@@ -16,7 +30,8 @@ urlpatterns = [
     path("genres/<int:pk>", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>", ActorDetail.as_view(), name="actor-detail"),
-
+    path("cinama-halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinama-halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail")
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,16 +1,16 @@
 from django.http import Http404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status, mixins, generics
+from rest_framework import status, mixins, generics, viewsets
 
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
-from cinema.models import Movie, Genre, Actor
+from cinema.models import Movie, Genre, Actor, CinemaHall
 from cinema.serializers import (
     MovieSerializer,
     GenreSerializer,
-    ActorSerializer
+    ActorSerializer, CinemaHallSerializer
 )
 
 
@@ -58,7 +58,8 @@ class GenreList(APIView):
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
+    @staticmethod
+    def post(request):
         serializer = GenreSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -70,7 +71,8 @@ class GenreList(APIView):
 
 
 class GenreDetail(APIView):
-    def get_object(self, pk):
+    @staticmethod
+    def get_object(pk):
         try:
             return Genre.objects.get(id=pk)
         except Genre.DoesNotExist:
@@ -79,7 +81,7 @@ class GenreDetail(APIView):
     def get(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
         genre = self.get_object(pk)
@@ -98,6 +100,7 @@ class GenreDetail(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
@@ -136,5 +139,20 @@ class ActorDetail(
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+
+class CinemaHallViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
