81787edb80f387dbef52cf101c8e118017709677
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-16 16:06:37 +0200

    Add cinema hall in path

--- cinema/urls.py
+++ cinema/urls.py
@@ -40,6 +40,16 @@ urlpatterns = [
         "actors/<int:pk>/",
         ActorDetail.as_view(),
         name="actor-detail"
+    ),
+    path(
+        "cinema_halls/",
+        cinema_hall_list,
+        name="cinema-hall-list"
+    ),
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail"
     )
 ]
 
