c3480d7e2d15f39d4b4e9d35471d36f9bb96d6a4
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-16 15:21:41 +0200

    Created GenericAPIView for ActorList

--- cinema/views.py
+++ cinema/views.py
@@ -1,13 +1,13 @@
 from django.http import Http404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, mixins
 
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
-from cinema.models import Movie, Genre
-from cinema.serializers import MovieSerializer, GenreSerializer
+from cinema.models import Movie, Genre, Actor
+from cinema.serializers import MovieSerializer, GenreSerializer, ActorSerializer
 
 
 @api_view(["GET", "POST"])
@@ -92,3 +92,14 @@ class GenreDetail(APIView):
         genre.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ActorList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def get(self, request, *args, **kwargs) -> list:
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
