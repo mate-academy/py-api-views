99a1d75e3212a05369efc4c1d0f79ab80c4d8b6b
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-16 17:22:16 +0200

    fixed url.py & fixed field name in CinemaHallSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -50,7 +50,7 @@ class CinemaHallSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
-        instance.rows = validated_data.get("raws", instance.rows)
+        instance.rows = validated_data.get("rows", instance.rows)
         instance.seats_in_row = validated_data.get(
             "seats_in_row", instance.seats_in_row
         )
--- cinema/urls.py
+++ cinema/urls.py
@@ -29,12 +29,12 @@ router.register("movies", MovieViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("genres/", GenreList.as_view(), name="genre-list"),
-    path("genres/<int:pk>", GenreDetail.as_view(), name="genre-detail"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
-    path("actors/<int:pk>", ActorDetail.as_view(), name="actor-detail"),
-    path("cinama-halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
-        "cinama-halls/<int:pk>/",
+        "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-hall-detail"
     )
