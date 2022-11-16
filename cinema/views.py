a9ba5aba4aef6e3bfde219d65e6318d8a55c23a3
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-16 15:04:08 +0200

    Created APIView for GenreDetail

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -61,3 +62,33 @@ class GenreList(APIView):
 
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
+
+class GenreDetail(APIView):
+    def get_object(self, pk: int):
+        try:
+            return Genre.objects.get(pk=pk)
+        except Genre.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk: int) -> Response:
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk: int) -> Response:
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk: int) -> Response:
+        genre = self.get_object(pk)
+        genre.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
