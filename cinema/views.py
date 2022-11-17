d18acd4f3077792422658b81b35374e928b88e2d
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-17 08:21:21 +0200

    Fix Actor detail view

--- cinema/views.py
+++ cinema/views.py
@@ -97,13 +97,13 @@ class ActorDetail(
         return self.retrieve(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+        return self.update(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+        return self.destroy(request, *args, **kwargs)
 
 
 class CinemaHallViewSet(
