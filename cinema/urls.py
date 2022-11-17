3ab8708d630892860f1d7dbcdc23b6e3d5dc2bdc
Ivan Antonishak <jus1stored@gmail.com>
2022-11-17 13:08:34 +0200

    Small fix urls and add serializer.errors

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import (
+    path,
+    include,
+)
+from rest_framework import (
+    routers,
+)
 
 from cinema.views import (
     MovieViewSet,
@@ -13,10 +18,13 @@ from cinema.views import (
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
 
-cinema_list = CinemaHallViewSet.as_view(
-    actions={"get": "list", "post": "create"}
+cinema_hall_list = CinemaHallViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create",
+    }
 )
-cinema_detail = CinemaHallViewSet.as_view(
+cinema_hall_detail = CinemaHallViewSet.as_view(
     actions={
         "get": "retrieve",
         "put": "update",
@@ -26,12 +34,36 @@ cinema_detail = CinemaHallViewSet.as_view(
 )
 
 urlpatterns = [
-    path("genres/", GenreList.as_view(), name="genre-list"),
-    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("actors/", ActorList.as_view(), name="actor-list"),
-    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema_halls/", cinema_list, name="cinema-list"),
-    path("cinema_halls/<int:pk>/", cinema_detail, name="cinema-detail"),
+    path(
+        "genres/",
+        GenreList.as_view(),
+        name="genre-list",
+    ),
+    path(
+        "genres/<int:pk>/",
+        GenreDetail.as_view(),
+        name="genre-detail",
+    ),
+    path(
+        "actors/",
+        ActorList.as_view(),
+        name="actor-list",
+    ),
+    path(
+        "actors/<int:pk>/",
+        ActorDetail.as_view(),
+        name="actor-detail",
+    ),
+    path(
+        "cinema_halls/",
+        cinema_hall_list,
+        name="cinema-hall-list",
+    ),
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail",
+    ),
     path("", include(router.urls)),
 ]
 
--- cinema/views.py
+++ cinema/views.py
@@ -30,7 +30,7 @@ class GenreList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GenreDetail(APIView):
@@ -52,7 +52,7 @@ class GenreDetail(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk):
         genre = self.get_object(pk=pk)
@@ -61,7 +61,7 @@ class GenreDetail(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         genre = self.get_object(pk=pk)
