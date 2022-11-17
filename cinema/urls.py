8e8b717138aaed58157e0bd9f89d5f644d64367c
Andriy Makarenko <andriy@mate.academy>
2022-11-17 17:10:46 +0200

    Change flake8

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from cinema.views import GenreList, GenreDetail, ActorList, ActorDetail, CinemaHallViewSet, MovieViewSet
+from cinema.views import (GenreList,
+                          GenreDetail,
+                          ActorList,
+                          ActorDetail,
+                          CinemaHallViewSet,
+                          MovieViewSet
+                          )
 
 cinema_hall_list = CinemaHallViewSet.as_view(actions={
     "get": "list",
     "post": "create",
-    })
+})
 cinema_hall_detail = CinemaHallViewSet.as_view(actions={
     "get": "retrieve",
     "put": "update",
@@ -24,7 +30,10 @@ urlpatterns = [
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path("cinema_halls/<int:pk>/",
+         cinema_hall_detail,
+         name="cinema-hall-detail",
+         ),
     path("", include(router.urls))
 ]
 
