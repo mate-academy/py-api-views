45aacbb571e8f073ff9a4a1e7d4e3134df69758a
stoveprofi <stoveprofi@gmail.com>
2022-11-19 18:19:31 +0000

    Solution. Serializer implemented

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,44 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    GenreList,
+    GenreDetail,
+    ActorList,
+    ActorDetail,
+    CinemaHallViewSet,
+    MovieViewSet
+)
+
+movie_list = MovieViewSet.as_view(actions={
+    "get": "list", "post": "create"
+})
+movie_detail = MovieViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
+
+cinema_hall_list = CinemaHallViewSet.as_view(actions={
+    "get": "list", "post": "create"
+})
+cinema_hall_detail = CinemaHallViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema-halls/<int:pk>/",
+         cinema_hall_detail, name="cinema-hall-detail"),
 ]
 
 app_name = "cinema"
