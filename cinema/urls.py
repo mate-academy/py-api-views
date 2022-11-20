8230a83c478da2db5b6aa50a2c83d5c1016bfb4c
graandda <graanddadays@gmail.com>
2022-11-20 16:54:16 +0200

    fixes5 - with flake8

--- cinema/urls.py
+++ cinema/urls.py
@@ -14,10 +14,7 @@ router = DefaultRouter()
 router.register("movies", MovieViewSet)
 
 cinema_hall_list = CinemaHallViewSet.as_view(
-    actions={
-        "get": "list",
-        "post": "create"
-    }
+    actions={"get": "list", "post": "create"}
 )
 cinema_hall_detail = CinemaHallViewSet.as_view(
     actions={
@@ -35,7 +32,9 @@ urlpatterns = [
     path("actors/", ActorCreateListView.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path(
+        "cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"
+    ),
 ]
 
 app_name = "cinema"
