0b4f56e7413961c6dab4b04e842f18be3953fc31
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-11-21 15:18:32 +0200

    Update models.py

--- cinema/models.py
+++ cinema/models.py
@@ -5,14 +5,14 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -29,5 +29,5 @@ class Movie(models.Model):
     genres = models.ManyToManyField(Genre, related_name="movies")
     duration = models.IntegerField()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
