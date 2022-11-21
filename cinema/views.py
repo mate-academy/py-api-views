ed9260f4cae1438c3d6ebe6e9d0ae8115553c166
Alexander <alexzubkoff123@gmail.com>
2022-11-21 18:45:59 +0200

    Refactored status of http responses

--- cinema/views.py
+++ cinema/views.py
@@ -51,7 +51,7 @@ def movie_detail(request, pk):
         serializer = MovieSerializer(movie, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -92,7 +92,7 @@ class GenreDetail(APIView):
         serializer = GenreSerializer(genre, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
