6e3f52607cf05067d469feb6c096b6e015e315f1
Dima Ovcharenko <kukuku381@gmail.com>
2022-11-22 12:00:10 +0200

    Fixed

--- cinema/models.py
+++ cinema/models.py
@@ -8,7 +8,7 @@ class Movie(models.Model):
     actors = models.ManyToManyField("Actor", related_name="movies")
     genres = models.ManyToManyField("Genre", related_name="movies")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
 
