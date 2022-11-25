e79a5e1920bccaf1407b1f14d3470f018a323ebe
Zakhar Podporinov <zp162162@gmail.com>
2022-11-26 00:52:45 +0200

    Add type annotations to models methods

--- cinema/models.py
+++ cinema/models.py
@@ -5,14 +5,14 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -21,7 +21,7 @@ class CinemaHall(models.Model):
     rows = models.PositiveIntegerField()
     seats_in_row = models.PositiveIntegerField()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.name} (rows: {self.rows}, " \
                f"seats in row: {self.seats_in_row}"
 
@@ -33,5 +33,5 @@ class Movie(models.Model):
     actors = models.ManyToManyField(Actor, related_name="movies")
     genres = models.ManyToManyField(Genre, related_name="movies")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
