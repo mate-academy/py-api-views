5839f10ce9777bba2d6dea536cf14919e2e4626b
Yurii <pazyn404@gmail.com>
2022-11-27 16:41:38 +0200

    Add str in models, refactor url

--- cinema/models.py
+++ cinema/models.py
@@ -16,12 +16,21 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=63)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
--- cinema/urls.py
+++ cinema/urls.py
@@ -11,37 +11,16 @@ from cinema.views import (
 )
 
 router = routers.DefaultRouter()
-router.register("movies", MovieViewSet)
 
-cinema_hall_list = CinemaHallViewSet.as_view(
-    actions={
-        "post": "create",
-        "get": "list",
-    }
-)
-
-cinema_hall_detail = CinemaHallViewSet.as_view(
-    actions={
-        "get": "retrieve",
-        "patch": "partial_update",
-        "put": "update",
-        "delete": "destroy",
-    }
-)
-
+router.register("movies", MovieViewSet, basename="movie-set")
+router.register("cinema_halls", CinemaHallViewSet, basename="cinema_halls-set")
 
 urlpatterns = [
-    path("", include(router.urls), name="movie"),
+    path("", include(router.urls)),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path(
-        "cinema_halls/<int:pk>/",
-        cinema_hall_detail,
-        name="cinema-hall-detail",
-    ),
 ]
 
 app_name = "cinema"
