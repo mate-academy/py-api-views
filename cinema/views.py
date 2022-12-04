573ec51cf57c382cd75bf8b6a060d6d6a6ff77a9
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-12-04 17:07:13 +0200

    Fixed Response in post method of GenreList(APIView)

--- cinema/views.py
+++ cinema/views.py
@@ -25,7 +25,7 @@ class GenreList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GenreDetail(APIView):
