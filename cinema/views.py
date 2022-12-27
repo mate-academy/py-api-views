74654fbb285570fddebb398cd30e5041bfdebb05
Danylchuk Maksym <maxitosm00@gmail.com>
2022-12-27 18:24:15 +0200

    ready

--- cinema/views.py
+++ cinema/views.py
@@ -101,7 +101,7 @@ class ActorDetail(
         return self.update(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+        return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
