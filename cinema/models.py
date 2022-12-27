9694f15d80d0caf88da13fd9fe12bf699923b12c
Danylchuk Maksym <maxitosm00@gmail.com>
2022-12-27 18:02:35 +0200

    ready

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,36 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return F"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    actors = models.ManyToManyField(to=Actor)
+    genres = models.ManyToManyField(to=Genre)
 
     def __str__(self):
         return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return F"{self.name} {self.rows} {self.seats_in_row}"
