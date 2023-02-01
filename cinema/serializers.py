8de02ff19e72b3cef778b920cd836c2bb21af508
Tetiana Omelchenko <t.omel4enko@gmail.com>
2023-02-01 21:39:53 +0800

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,24 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Genre, Actor, CinemaHall
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
 
 
 class MovieSerializer(serializers.Serializer):
@@ -8,6 +26,8 @@ class MovieSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255, required=True)
     description = serializers.CharField(required=True)
     duration = serializers.IntegerField(required=True)
+    actors = ActorSerializer(read_only=True, many=True)
+    genres = GenreSerializer(read_only=True, many=True)
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
@@ -18,7 +38,5 @@ class MovieSerializer(serializers.Serializer):
             "description", instance.description
         )
         instance.duration = validated_data.get("duration", instance.duration)
-
         instance.save()
-
         return instance
