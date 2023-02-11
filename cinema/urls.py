910b1b5d985aedfb4e3ad67fa60a843bc106ec35
Diana <daria.blessed18@gmail.com>
2023-02-12 00:34:02 +0200

    Crete CinemaHall views

--- cinema/urls.py
+++ cinema/urls.py
@@ -7,7 +7,19 @@ from cinema.views import (
     GenreDetail,
     ActorList,
     ActorDetail,
+    CinemaHallViewSet,
 )
+
+cinema_hall_list = CinemaHallViewSet.as_view(actions={
+            "get": "list",
+            "post": "create",
+        })
+cinema_hall_detail = CinemaHallViewSet.as_view(actions={
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+            "delete": "destroy",
+        })
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
@@ -16,6 +28,8 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema-halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,15 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, mixins, viewsets
 
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
-from cinema.models import Movie, Genre, Actor
+from cinema.models import Movie, Genre, Actor, CinemaHall
 from cinema.serializers import (
     MovieSerializer,
     GenreSerializer,
-    ActorSerializer
+    ActorSerializer,
+    CinemaHallSerializer
 )
 
 
@@ -95,3 +96,15 @@ class ActorList(generics.ListCreateAPIView):
 class ActorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
