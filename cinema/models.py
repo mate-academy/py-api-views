873f8a6edffa5e4dd3ed5da57b52701c6ede6f3a
Vika <viktorykoroliova@gmail.com>
2023-02-13 20:59:47 +0200

    solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=55)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    genres = models.ManyToManyField(to=Genre, related_name="movies")
+    actors = models.ManyToManyField(to=Actor, related_name="movies")
 
     def __str__(self):
         return self.title
--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-ru6ndfka@95_(lysua8yhdjq@vpiqgv3yru4r)q3h4_u8x7dfy"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
