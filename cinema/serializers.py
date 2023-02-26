e05a3b38c22ee0f74688bd73069e0201cc45863e
Denys Tereshchuk <terrya@ukr.net>
2023-02-26 21:24:23 +0400

    Added serializers for Genre, Actor, CinemaHall, Movie

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,24 +1,23 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Actor, CinemaHall, Genre
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255, required=True)
-    description = serializers.CharField(required=True)
-    duration = serializers.IntegerField(required=True)
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get(
-            "description", instance.description
-        )
-        instance.duration = validated_data.get("duration", instance.duration)
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
 
-        instance.save()
 
-        return instance
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
