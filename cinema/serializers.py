bdf4d22c117d3aac90a297565fcb718c4c97bd19
Denys Tereshchuk <terrya@ukr.net>
2023-02-26 21:32:20 +0400

    Added fields to all serializers

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,18 +6,22 @@ from cinema.models import Movie, Actor, CinemaHall, Genre
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
+        fields = "__all__"
 
 
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
+        fields = "__all__"
 
 
 class CinemaHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = CinemaHall
+        fields = "__all__"
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
+        fields = "__all__"
