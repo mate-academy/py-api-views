662571297474280b119d40a2454adf806f16f0c8
Denys Tereshchuk <terrya@ukr.net>
2023-02-26 21:53:39 +0400

    Added paths for Genre in urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import GenreList, GenreDetail, MovieViewSet, ActorList, \
+    ActorDetail
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    # path("actors/", ActorList.as_view(), name="genre-detail"),
+    # path("genres/<int:pk>/", ActorDetail.as_view(), name="genre-detail"),
+
+    # path("movies/", MovieViewSet.as_view(), name="movie-list"),
+    # path("movies/<int:pk>/", MovieViewSet.as_view(), name="movie-detail"),
 ]
 
 app_name = "cinema"
