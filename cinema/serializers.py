044898a0b272c129a01ab96c89cd58d44fabd782
Denys Tereshchuk <terrya@ukr.net>
2023-02-26 22:30:13 +0400

    Changes fields actors & genres in MovieSerializers

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from cinema.models import Movie, Actor, CinemaHall, Genre
 
 
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = "__all__"
-
-
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
@@ -24,4 +18,13 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
+        fields = "__all__"
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many=True, required=False)
+    genres = GenreSerializer(many=True, required=False)
+
+    class Meta:
+        model = Movie
         fields = "__all__"
