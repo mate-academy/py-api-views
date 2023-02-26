4c3b3e8a452d197e95c3f7829599515e8cf5c73c
Denys Tereshchuk <terrya@ukr.net>
2023-02-26 21:58:42 +0400

    Added urls for Actor

--- cinema/urls.py
+++ cinema/urls.py
@@ -7,8 +7,8 @@ from cinema.views import GenreList, GenreDetail, MovieViewSet, ActorList, \
 urlpatterns = [
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    # path("actors/", ActorList.as_view(), name="genre-detail"),
-    # path("genres/<int:pk>/", ActorDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
 
     # path("movies/", MovieViewSet.as_view(), name="movie-list"),
     # path("movies/<int:pk>/", MovieViewSet.as_view(), name="movie-detail"),
