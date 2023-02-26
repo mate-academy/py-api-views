5f21475c9a5bdbeb4d3b8131e2d2cc3d7c3ed8ae
Denys Tereshchuk <terrya@ukr.net>
2023-02-26 22:15:34 +0400

    Changed endpoint's name from cinemahalls to cinema_halls

--- cinema/urls.py
+++ cinema/urls.py
@@ -18,11 +18,11 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinemahalls/", CinemaHallViewSet.as_view(actions={
+    path("cinema_halls/", CinemaHallViewSet.as_view(actions={
         "get": "list",
         "post": "create",
     }), name="cinemahall-list"),
-    path("cinemahalls/<int:pk>/", CinemaHallViewSet.as_view(actions={
+    path("cinema_halls/<int:pk>/", CinemaHallViewSet.as_view(actions={
         "get": "retrieve",
         "put": "update",
         "patch": "partial_update",
