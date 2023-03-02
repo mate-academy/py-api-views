4385856a6b48fb6a054cf84f893f84fd2e6f849a
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-03-02 23:37:06 +0200

    added path for all pages

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,22 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import MovieViewSet, CinemaHallViewSet, ActorList, ActorDetail, GenreList, GenreDetail
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("cinema-halls", CinemaHallViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls)),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path(
+        "actors/<int:pk>/",
+        ActorDetail.as_view(),
+        name="actor-detail"
+    ),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail")
 ]
 
 app_name = "cinema"
