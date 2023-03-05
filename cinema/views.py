402c14e3356b78eedb9b5ec9b63c6c7f96397baa
Mykola Zakharov <kolyazakharov94@gmail.com>
2023-03-05 19:49:05 +0200

    added type annotation

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from rest_framework import status, viewsets, mixins, generics
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
@@ -37,10 +39,10 @@ class ActorList(
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request: Any, *args, **kwargs) -> Response:
         return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request: Any, *args, **kwargs) -> Response:
         return self.create(request, *args, **kwargs)
 
 
@@ -53,26 +55,27 @@ class ActorDetail(
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request: Any, *args, **kwargs) -> Response:
         return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
+    def put(self, request: Any, *args, **kwargs) -> Response:
         return self.update(request, *args, **kwargs)
 
-    def patch(self, request, *args, **kwargs):
+    def patch(self, request: Any, *args, **kwargs) -> Response:
         return self.partial_update(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
+    def delete(self, request: Any, *args, **kwargs) -> Response:
         return self.destroy(request, *args, **kwargs)
 
 
 class GenreList(APIView):
-    def get(self, request):
+
+    def get(self, request: Any) -> Response:
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
+    def post(self, request: Any) -> Response:
         serializer = GenreSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -81,30 +84,30 @@ class GenreList(APIView):
 
 class GenreDetail(APIView):
 
-    def get_object(self, pk):
+    def get_object(self, pk: int) -> Genre:
         return get_object_or_404(Genre, pk=pk)
 
-    def get(self, request, pk):
+    def get(self, request: Any, pk: int) -> Response:
         genre = self.get_object(pk)
 
         serializer = GenreSerializer(genre)
         return Response(serializer.data)
 
-    def put(self, request, pk):
+    def put(self, request: Any, pk: int) -> Response:
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-    def patch(self, request, pk):
+    def patch(self, request: Any, pk: int) -> Response:
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-    def delete(self, request, pk):
+    def delete(self, request: Any, pk: int) -> Response:
         genre = self.get_object(pk)
         genre.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
