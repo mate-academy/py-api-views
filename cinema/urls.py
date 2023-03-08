18ed8df586edd9869a7de88e5e1dfdd0ce99e7e7
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-08 21:03:16 +0100

    Overwrite Movie views functions on CBV with viewsets

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from cinema.views import MovieViewSet
 
-from cinema.views import movie_list, movie_detail
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls))
 ]
 
 app_name = "cinema"
