cdc7bfc03cd198e4f06db4f377f91cae8e2862fb
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-09 15:20:43 +0100

    Added GenreListView & GenreDetailView

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
-from cinema.views import MovieViewSet, CinemaHallViewSet
+from cinema.views import (
+    MovieModelViewSet,
+    CinemaHallViewSet,
+    ActorListView,
+    ActorDetailView, GenreListView, GenreDetailView
+)
 
 router = routers.DefaultRouter()
-router.register("movies", MovieViewSet)
+router.register("movies", MovieModelViewSet)
 
 cinema_hall_list = CinemaHallViewSet.as_view(
     actions={
@@ -25,6 +30,10 @@ urlpatterns = [
     path("", include(router.urls)),
     path("cinema-hall/", cinema_hall_list, name="cinema-hall"),
     path("cinema-hall/<int:pk>/", cinema_hall_detail, name="cinema-detail"),
+    path("actors/", ActorListView.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
+    path("genres/", GenreListView.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetailView.as_view(), name="genre-detail"),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,15 @@
-from rest_framework import viewsets, mixins, generics
-from cinema.models import Movie, CinemaHall, Actor
-from cinema.serializers import MovieSerializer, CinemaHallSerializer
+from rest_framework import viewsets, mixins, generics, status
+from django.shortcuts import get_object_or_404
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from cinema.models import Movie, CinemaHall, Actor, Genre
+from cinema.serializers import (
+    MovieSerializer,
+    CinemaHallSerializer,
+    ActorSerializer, GenreSerializer
+)
 
 
 class MovieModelViewSet(viewsets.ModelViewSet):
@@ -18,3 +27,74 @@ class CinemaHallViewSet(
 ):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
+
+
+class ActorListView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    model = Actor
+    serializer_class = ActorSerializer
+    queryset = Actor.objects.all()
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        return self.create(request, *args, **kwargs)
+
+
+class ActorDetailView(
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView
+):
+    model = Actor
+    serializer_class = ActorSerializer
+    queryset = Actor.objects.all()
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        return self.retrieve(request, *args, **kwargs)
+
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request: Request, *args, **kwargs) -> Response:
+        return self.destroy(request, *args, **kwargs)
+
+
+class GenreListView(APIView):
+    def get(self, request: Request) -> Response:
+        genre = Genre.objects.all()
+        serializer = GenreSerializer(genre, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request: Request) -> Response:
+        serializer = GenreSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class GenreDetailView(APIView):
+    def get_object(self, request: Request, pk: int) -> Genre:
+        return get_object_or_404(Genre, pk=pk)
+
+    def get(self, request: Request, pk: int) -> Response:
+        genre = self.get_object(request, pk)
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request: Request, pk: int) -> Response:
+        genre = self.get_object(request, pk)
+        serializer = GenreSerializer(genre, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request: Request, pk: int) -> Response:
+        genre = self.get_object(request, pk)
+        genre.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
