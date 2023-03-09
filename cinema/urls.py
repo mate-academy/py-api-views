11d7c0fd8c5fcd8c70b047f2e3e67f1dd6809096
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-09 14:44:13 +0100

    Added CinemaHallViewSet with urls pattern

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,12 +1,30 @@
 from django.urls import path, include
 from rest_framework import routers
-from cinema.views import MovieViewSet
+from cinema.views import MovieViewSet, CinemaHallViewSet
 
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
 
+cinema_hall_list = CinemaHallViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create"
+    }
+)
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
+)
+
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("", include(router.urls)),
+    path("cinema-hall/", cinema_hall_list, name="cinema-hall"),
+    path("cinema-hall/<int:pk>/", cinema_hall_detail, name="cinema-detail"),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,8 +1,20 @@
-from rest_framework import viewsets
-from cinema.models import Movie
-from cinema.serializers import MovieSerializer
+from rest_framework import viewsets, mixins, generics
+from cinema.models import Movie, CinemaHall, Actor
+from cinema.serializers import MovieSerializer, CinemaHallSerializer
 
 
-class MovieViewSet(viewsets.ModelViewSet):
+class MovieModelViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+
+class CinemaHallViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
