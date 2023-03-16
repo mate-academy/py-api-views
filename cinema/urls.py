42a68e82057aba7a0470d02f36e4bf646cbf8230
Ivan Mayor <maramorosh2@gmail.com>
2023-03-16 16:21:10 +0200

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,25 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    MovieSet,
+    GenreListView,
+    GenreDetailView,
+    ActorListView,
+    ActorDetailView,
+    CinemaHallViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("movies/", MovieSet)
+router.register("cinema_halls", CinemaHallViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls)),
+    path("genres/", GenreListView.as_view(), name="genres-list"),
+    path("genres/<int:pk>/", GenreDetailView.as_view(), name="genres-detail"),
+    path("actors/", ActorListView.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
 ]
 
 app_name = "cinema"
