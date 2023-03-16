cc8b608aa8666870d5e494ec5d94f82c4091107d
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-03-16 14:18:47 +0200

    APIViews

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from cinema.models import Movie
+from cinema.models import Movie, Genre, CinemaHall, Actor
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(Genre)
+admin.site.register(CinemaHall)
+admin.site.register(Actor)
