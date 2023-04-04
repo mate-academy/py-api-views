b0e32bd49d9eb5ea153636dbe8fb7a3cc8a65b30
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-04 17:37:11 +0300

    add Actor, Genre, CinemaHall, Movie models according to readme

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,46 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=127)
+    last_name = models.CharField(max_length=127)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.SmallIntegerField()
+    seats_in_row = models.SmallIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(
+        Actor,
+        related_name="movies"
+    )
+    genres = models.ManyToManyField(
+        Genre,
+        related_name="genres"
+    )
     duration = models.IntegerField()
 
     def __str__(self):
         return self.title
+
