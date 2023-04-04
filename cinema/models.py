822824f809699986891c3b8e3197215288edb4ff
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-04-04 10:55:32 +0300

    add Genre, Actor, CinemaHall models and update Movie model

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,51 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["first_name"]
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=64)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return (
+            f"{self.name} "
+            f"(rows: {self.rows}, seats in row: {self.seats_in_row})"
+        )
+
+    class Meta:
+        ordering = ["name"]
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
     duration = models.IntegerField()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
+
+    class Meta:
+        ordering = ["title"]
