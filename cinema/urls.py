18503ad601b5fbf90df1813c44ea7211f9b3fabe
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-05 03:09:24 +0300

    refactor urls according to readme

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from cinema.views import (
     ActorDetail,
@@ -6,12 +7,17 @@ from cinema.views import (
     CinemaHallViewSet,
     GenreDetail,
     GenreList,
+    MovieViewSet,
 )
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("movie", MovieViewSet)
 
 cinema_hall_list = CinemaHallViewSet.as_view(
     actions={
         "get": "list",
-        "post": "create"
+        "post": "create",
     })
 cinema_hall_detail = CinemaHallViewSet.as_view(
     actions={
@@ -25,13 +31,14 @@ urlpatterns = [
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
-    path("actors/<int:pk>", ActorDetail.as_view()),
+    path("actors/<int:pk>", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-halls-list"),
     path(
         "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-halls-list"
-    )
+    ),
+    path("", include(router.urls))
 ]
 
 app_name = "cinema"
