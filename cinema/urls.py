4b0d948c9358056aa6a55059a0d92fbd6d69027f
ostapix@gmail.com <@Bandera345>
2023-04-05 19:17:31 +0300

    rolled back the function based view for movie because the tests did not pass

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 from rest_framework import routers
 
 from cinema.views import (
-    movie_list,
-    movie_detail,
     GenreList,
     GenreDetail,
     ActorList,
