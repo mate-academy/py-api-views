45ef072cc7ad0fbd2934ca775fc021deb7537e71
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-04-05 14:10:21 +0300

    fix flake8

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from cinema.views import(
+from cinema.views import (
     GenreList,
     GenreDetail,
     ActorList,
