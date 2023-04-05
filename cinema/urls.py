318b7b5bf0be9407faa8f68d87da96decf39d9d2
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-05 23:45:49 +0300

    update flake8

--- cinema/urls.py
+++ cinema/urls.py
@@ -17,7 +17,12 @@ router.register("movies", MovieViewSet)
 cinema_hall_list = CinemaHallViewSet.as_view({"get": "list", "post": "create"})
 
 cinema_hall_detail = CinemaHallViewSet.as_view(
-    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
+    {
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
 )
 
 urlpatterns = [
@@ -26,7 +31,9 @@ urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path(
+        "cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"
+    ),
     path("", include(router.urls)),
 ]
 
