d7574cd6c176ed67c0082d984f589b7e73ade2b8
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-04-06 18:43:28 +0300

    fix get_object

--- cinema/views.py
+++ cinema/views.py
@@ -33,10 +33,7 @@ class GenreList(APIView):
 
 class GenreDetail(APIView):
     def get_object(self, pk: int) -> Genre:
-        try:
-            return Genre.objects.get(pk=pk)
-        except Genre.DoesNotExist:
-            raise Http404
+        return get_object_or_404(Genre, pk=pk)
 
     def get(self, request, pk) -> Response:
         genre = self.get_object(pk)
