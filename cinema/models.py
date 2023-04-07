82fbd66ee2161b375c1fa00ca49633ef6bb8ef6e
kuznetsb <kuznetsbog@gmail.com>
2023-04-07 10:11:54 +0300

    Str for genre added

--- cinema/models.py
+++ cinema/models.py
@@ -12,6 +12,9 @@ class Actor(models.Model):
 class Genre(models.Model):
     name = models.CharField(max_length=63, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
