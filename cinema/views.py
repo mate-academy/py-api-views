f362c4ddaed4fbe310c37097c23725a6aa480f78
Artur Tytomyr <art.tytomyr@gmail.com>
2023-04-07 09:49:27 +0300

    revised

--- cinema/views.py
+++ cinema/views.py
@@ -33,14 +33,14 @@ class GenreDetail(APIView):
     def get(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk):
