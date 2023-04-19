4d525a1b90cbbe8cd44a11422138bf507ba45395
Arsen Makovei <makoveyarsen@gmail.com>
2023-04-19 21:44:17 +0300

    Fixed mistakes in urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -22,16 +22,16 @@ cinema_hall_detail = CinemaHallViewSet.as_view(actions={
 })
 
 router = routers.DefaultRouter()
-router.register("movie", MovieViewSet)
+router.register("movies", MovieViewSet)
 
 urlpatterns = [
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actors-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actors-detail"),
-    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
-        "cinema-halls/<int:pk>/",
+        "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-hall-detail"
     ),
