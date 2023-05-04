ae5a09c3c150002bf08e36cef6224c04ba0c8e86
Paul Maslov <maslovp67@gmail.com>
2023-05-04 21:41:19 +0300

    Added ModelViewSet for model Movie

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from cinema.views import (
-    movie_list, movie_detail,
     GenreList, GenreDetail,
     ActorList, ActorDetail,
-    CinemaHallViewSet
+    CinemaHallViewSet, MovieViewSet
 )
 
 
@@ -23,9 +23,12 @@ cinema_hall_detail = CinemaHallViewSet.as_view(
     }
 )
 
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+
+
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls)),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
@@ -35,3 +38,4 @@ urlpatterns = [
 ]
 
 app_name = "cinema"
+
