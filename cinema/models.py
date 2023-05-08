0801769166beb36daa96be209095d1de173d9527
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-05-08 07:46:22 +0300

    Add viesw and urls for all models

--- cinema/models.py
+++ cinema/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 
 class Actor(models.Model):
-    first_name = models.CharField(max_length=63)
-    last_name = models.CharField(max_length=63)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=63, unique=True)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class CinemaHall(models.Model):
-    name = models.CharField(max_length=63)
+    name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
