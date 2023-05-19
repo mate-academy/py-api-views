bc1a279f6c76098a574a1259d309d5e1e0c85641
Oleksandr Mamchur <irex1988@gmail.com>
2023-05-19 16:00:08 +0300

    fix typehints

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -8,10 +8,10 @@ class ActorSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=255)
     last_name = serializers.CharField(max_length=255)
 
-    def create(self, validated_data) -> object:
+    def create(self, validated_data) -> Actor:
         return Actor.objects.create(**validated_data)
 
-    def update(self, instance, validated_data) -> object:
+    def update(self, instance, validated_data) -> Actor:
         instance.first_name = validated_data.get(
             "first_name",
             instance.first_name
@@ -29,10 +29,10 @@ class GenreSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
 
-    def create(self, validated_data) -> object:
+    def create(self, validated_data) -> Genre:
         return Genre.objects.create(**validated_data)
 
-    def update(self, instance, validated_data) -> object:
+    def update(self, instance, validated_data) -> Genre:
         instance.name = validated_data.get("name", instance.name)
         instance.save()
 
@@ -45,10 +45,10 @@ class CinemaHallSerializer(serializers.Serializer):
     rows = serializers.IntegerField()
     seats_in_row = serializers.IntegerField()
 
-    def create(self, validated_data) -> object:
+    def create(self, validated_data) -> CinemaHall:
         return CinemaHall.objects.create(**validated_data)
 
-    def update(self, instance, validated_data) -> object:
+    def update(self, instance, validated_data) -> CinemaHall:
         instance.name = validated_data.get("name", instance.name)
         instance.rows = validated_data.get("rows", instance.rows)
         instance.seats_in_row = validated_data.get(
