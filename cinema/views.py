38805af55df702429c85fcf2b0bace919eef1b4b
Oleksandr Mamchur <irex1988@gmail.com>
2023-05-19 15:24:06 +0300

    fix tests

--- cinema/views.py
+++ cinema/views.py
@@ -83,7 +83,10 @@ class ActorDetail(
         return self.retrieve(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
