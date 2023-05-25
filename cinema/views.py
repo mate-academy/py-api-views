9f37288af6660a425e1c48d9f8843b610c0409e9
Amirammi <amirammina@gmail.com>
2023-05-25 21:46:44 +0300

    Add raise_exeption in is_valid()

--- cinema/views.py
+++ cinema/views.py
@@ -22,10 +22,9 @@ class GenreList(APIView):
     def post(self, request):
         serializer = GenreSerializer(data=request.data)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GenreDetail(APIView):
@@ -41,19 +40,17 @@ class GenreDetail(APIView):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data, partial=True)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         genre = self.get_object(pk)
