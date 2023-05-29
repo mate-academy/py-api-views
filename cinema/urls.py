7a657dfe1ea1ceb6ee1912269213f74410864026
soobig666 <darkvader.ukr.net@gmail.com>
2023-05-29 14:01:19 +0300

    Upd rewrote new urls in urls.py with new models

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import GenreView, ActorView, CinemaHallView, MovieView
 
-from cinema.views import movie_list, movie_detail
+router = DefaultRouter()
+router.register(r"movies", MovieView, basename="movie")
+router.register(r"cinema_halls", CinemaHallView, basename="cinema_hall")
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreView.as_view()),
+    path("actors/", ActorView.as_view()),
+    path("", include(router.urls)),
 ]
+
 
 app_name = "cinema"
