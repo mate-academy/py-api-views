751220c27b426612fbe0bfd5b86a2371ef03f744
Artem Motrunich <motrunich2013@gmail.com>
2023-06-21 19:41:36 +0200

    Fix if-statement

--- cinema/views.py
+++ cinema/views.py
@@ -32,11 +32,11 @@ class GenreList(APIView):
 
     def post(self, request):
         serializer = GenreSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class GenreDetail(APIView):
@@ -50,11 +50,11 @@ class GenreDetail(APIView):
     def put(self, request, pk):
         genre = get_object_or_404(Genre, id=pk)
         serializer = GenreSerializer(genre, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request, pk):
         genre = get_object_or_404(Genre, id=pk)
