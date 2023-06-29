9ad29b6d2b57fe14b51ec83d137828009f1955f7
Petro Abramov <sebshe3@gmail.com>
2023-06-30 01:32:19 +0300

    Rewrite parrents class for CinemaHallViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -109,7 +109,7 @@ class CinemaHallViewSet(
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
-    generics.GenericAPIView
+    viewsets.ModelViewSet
 ):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
