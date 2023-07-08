662324f411ebdf14e4453323f7c4c24ef41e03bc
ZaikaDaria <corplinebusiness@gmail.com>
2023-07-08 16:24:47 +0300

    Rewrite router in url.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -22,19 +22,15 @@ cinema_hall_detail = CinemaHallViewSet.as_view(actions={
 })
 
 router = routers.DefaultRouter()
-router.register("movies", MovieViewSet)
+router.register("cinema_halls", CinemaHallViewSet, basename="cinema_hall")
+router.register("movies", MovieViewSet, basename="movie")
 
 urlpatterns = [
-    path("genres/", GenreList.as_view(), name="genre-list"),
-    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("actors/", ActorList.as_view(), name="actor-list"),
-    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema_halls/", cinema_hall_list, name="cinema_hall-list"),
-    path("cinema_halls/<int:pk>/",
-         cinema_hall_detail,
-         name="cinema_hall-detail"
-         ),
-    path("", include(router.urls)),
-]
+                  path("genres/", GenreList.as_view(), name="genre-list"),
+                  path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+                  path("actors/", ActorList.as_view(), name="actor-list"),
+                  path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+
+              ] + router.urls
 
 app_name = "cinema"
