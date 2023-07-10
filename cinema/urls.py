df46beba3fe975e68c6f81b422cb4b10370fdf4c
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-07-10 17:14:27 +0300

    change url cinema-hall to router

--- cinema/urls.py
+++ cinema/urls.py
@@ -10,32 +10,33 @@ from cinema.views import (
     ActorDetail,
     CinemaHallViewSet, MovieViewSet,
 )
-cinema_hall_list = CinemaHallViewSet.as_view(
-    actions={
-        "get": "list",
-        "post": "create"
-    })
-cinema_hall_detail = CinemaHallViewSet.as_view(
-    actions={
-        "get": "retrieve",
-        "put": "update",
-        "patch": "partial_update",
-        "delete": "destroy"
-    })
+# cinema_hall_list = CinemaHallViewSet.as_view(
+#     actions={
+#         "get": "list",
+#         "post": "create"
+#     })
+# cinema_hall_detail = CinemaHallViewSet.as_view(
+#     actions={
+#         "get": "retrieve",
+#         "put": "update",
+#         "patch": "partial_update",
+#         "delete": "destroy"
+#     })
 
 router = routers.DefaultRouter()
-router.register("movie", MovieViewSet)
+router.register("movies", MovieViewSet)
+router.register("cinema_hall", CinemaHallViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("movie/", movie_list, name="movie-list"),
+    path("movie/<int:pk>/", movie_detail, name="movie-detail"),
     path("genre/", GenreList.as_view(), name="genre-list"),
     path("genre/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actors-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema_hall/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_hall/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
-    path("movie/", include(router.urls))
+    # path("cinema_hall/", cinema_hall_list, name="cinema-hall-list"),
+    # path("cinema_hall/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path("", include(router.urls))
 ]
 
 app_name = "cinema"
