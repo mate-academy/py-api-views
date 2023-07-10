0805a124008f2dd6c775671afa0eb2d65156f8b8
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-07-10 12:53:40 +0300

    created Genre CRUD APIView

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import movie_list, movie_detail, GenreLIst, GenreDetail
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genre/", GenreLIst.as_view(), name="genre-list"),
+    path("genre/<int:pk>/", GenreDetail.as_view, name="genre-detail"),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
-from cinema.models import Movie
-from cinema.serializers import MovieSerializer
+from cinema.models import Movie, Genre
+from cinema.serializers import MovieSerializer, GenreSerializer
 
 
 @api_view(["GET", "POST"])
@@ -43,3 +44,42 @@ def movie_detail(request, pk):
     if request.method == "DELETE":
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class GenreLIst(APIView):
+    def get(self, request):
+        genre = Genre.objects.all()
+        serializer = GenreSerializer(genre, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = GenreSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class GenreDetail(APIView):
+    def get(self, request, pk):
+        genre = get_object_or_404(Genre, pk=pk)
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        genre = get_object_or_404(Genre, pk=pk)
+        serializer = GenreSerializer(genre, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def patch(self, request, pk):
+        genre = get_object_or_404(Genre, pk=pk)
+        serializer = GenreSerializer(genre, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def delete(self, request, pk):
+        genre = get_object_or_404(Genre, pk=pk)
+        genre.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
