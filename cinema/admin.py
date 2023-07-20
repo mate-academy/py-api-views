38eef078d189ed6436180e978af5e822608ce8e7
Anna Blahitka <anettaabl@gmail.com>
2023-07-20 11:05:30 +0300

    Solution

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-from cinema.models import Movie
+from cinema.models import Movie, Genre, Actor, CinemaHall
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("title", "duration")
+    list_filter = ("genres", "actors")
+    search_fields = ("title", "description")
+    filter_horizontal = ("genres", "actors")
+
+
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(CinemaHall)
--- cinema/models.py
+++ cinema/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=64)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
     duration = models.IntegerField()
 
     def __str__(self):
