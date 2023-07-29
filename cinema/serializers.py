2542753f521aec3c5b24b3efc3275667e6f360c9
yakhlamova <yanaakhlamova@gmail.com>
2023-07-29 17:01:56 +0300

    'Update'

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -82,7 +82,7 @@ class CinemaHallSerializer(serializers.Serializer):
         instance.name = validated_data.get("name", instance.name)
         instance.rows = validated_data.get("rows", instance.rows)
         instance.seats_in_row = validated_data.get(
-            "seats_in_rows", instance.seats_in_row
+            "seats_in_row", instance.seats_in_row
         )
 
         instance.save()
