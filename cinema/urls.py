5c4925acbea767a1446bb0d5b4acaf392a2cc856
Bohdan Liutko <luitko007@gmail.com>
2023-08-05 16:57:31 +0000

    Updated views and urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -25,7 +25,6 @@ cinema_hall_detail = CinemaHallViewSet.as_view({
 
 
 urlpatterns = [
-    path("", include(router.urls)),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema_hall-list"),
@@ -34,6 +33,6 @@ urlpatterns = [
          name="cinema_hall-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-]
+] + router.urls
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -40,11 +40,10 @@ class GenreDetail(APIView):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request, pk):
         genre = self.get_object(pk)
