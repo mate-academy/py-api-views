18dc7c6f14054845621b04ac6c1c3dbe0013ea7f
Vitalii2906 <fahivchik1@gmail.com>
2023-08-07 14:07:30 +0300

    Solution2

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -20,10 +20,29 @@ class CinemaHallSerializer(serializers.ModelSerializer):
         fields = ["name", "rows", "seats_in_row"]
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
     actors = ActorSerializer(many=True)
     genres = GenreSerializer(many=True)
 
     class Meta:
         model = Movie
         fields = ["title", "description", "duration", "actors", "genres"]
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get(
+            "description",
+            instance.description
+        )
+        instance.duration = validated_data.get("duration", instance.duration)
+
+        instance.save()
+
+        return instance
