a00eb86293c6bf414e5aa5d862b7cd2c855ffeec
Vitalii2906 <fahivchik1@gmail.com>
2023-08-07 15:04:08 +0300

    Solution5

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -25,7 +25,10 @@ class CinemaHallSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
         instance.rows = validated_data.get("rows", instance.rows)
-        instance.seats_in_row = validated_data.get("seats_in_row", instance.seats_in_row)
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row",
+            instance.seats_in_row
+        )
         instance.save()
         return instance
 
