760e1ed81101224942346d736f539f40187360b3
Vitalii2906 <fahivchik1@gmail.com>
2023-08-07 16:24:54 +0300

    Solution7

--- cinema/views.py
+++ cinema/views.py
@@ -30,10 +30,9 @@ class GenreList(APIView):
 
     def post(self, request):
         serializer = GenreSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class GenreDetail(APIView):
