e2a8652198e1d29d8fdd63da364801ee02cbef11
Taras Savchyn <savik1992@gmail.com>
2023-08-10 23:34:14 +0300

    add views test

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,33 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import MovieViewSet, CinemaHallViewSet, GenreList, GenreDetail, ActorList, ActorDetail
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls)),
+    path("cinemahalls/", CinemaHallViewSet.as_view(
+        actions={
+            "get": "list",
+            "post": "create",
+        }
+    ), name="cinema-hall-list"),
+
+    path("cinemahalls/<int:pk>/", CinemaHallViewSet.as_view(
+        actions={
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+            "delete": "destroy",
+        }
+    ), name="cinema-hall-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("genres/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
 ]
 
-app_name = "cinema"
+app_name = "station"
+
