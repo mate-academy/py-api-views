d5dc2a635fdc75cde1f9ff830f47e1ead4fc6a21
Alexandr Nikiforenko <NikiforenkoAS@i.ua>
2023-08-11 18:00:43 +0300

    Added def patch in class GenreDetail

--- cinema/views.py
+++ cinema/views.py
@@ -59,6 +59,21 @@ class GenreDetail(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
+    def patch(self, request, pk):
+        genre = self.get_object(pk=pk)
+        serializer = GenreSerializer(genre, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
     def delete(self, request, pk):
         genre = self.get_object(pk=pk)
         genre.delete()
