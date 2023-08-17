0fad02f10b5da31f3690ad69de0001f971bea702
Sashkoooo <o.denysenko@hotmail.com>
2023-08-17 18:33:10 +0300

    Implement cinema_hall views

---
+++ cinema/views/cinema_hall_views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets, mixins
+
+from cinema.models import CinemaHall
+from cinema.serializers import CinemaHallSerializer
+
+
+class CinemaHallViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
