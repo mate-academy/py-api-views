78701d8ddd7b6259ebc69734cf2a5f04e59564ce
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-08-24 22:56:22 +0300

    add models Genre, Actors, CinemaHall , make migrations, views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,31 @@
-from django.urls import path
+from django.urls import path, include
 
-from cinema.views import movie_list, movie_detail
+from rest_framework import routers
+from cinema.views import (
+    GenreList,
+    GenreDetail,
+    ActorList,
+    ActorDetail,
+    CinemaHallList,
+    CinemaHallDetail,
+    MoviesViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("movies", MoviesViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genres-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genres-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", CinemaHallList.as_view(), name="cinema_halls-list"),
+    path(
+        "cinema_halls/<int:pk>/",
+        CinemaHallDetail.as_view(),
+        name="cinema_halls-detail"
+    ),
+    path("", include(router.urls)),
 ]
 
 app_name = "cinema"
