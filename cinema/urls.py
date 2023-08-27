0426e03029d81b04faff5fb4659661d07ffc1fb1
misha <mishabenzel1997@gmail.com>
2023-08-27 14:36:58 +0200

    'Solution'

--- cinema/urls.py
+++ cinema/urls.py
@@ -16,7 +16,7 @@ cinema_hall_list = CinemaHallViewSet.as_view(actions={
 })
 cinema_hall_detail = CinemaHallViewSet.as_view({
     "get": "retrieve",
-    "post": "update",
+    "put": "update",
     "patch": "partial_update",
     "delete": "destroy"
 })
@@ -29,9 +29,9 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema_hall/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
-        "cinema_hall/<int:pk>/",
+        "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-hall-detail"),
     path("", include(router.urls))
--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status, generics, mixins, viewsets
 
@@ -28,22 +29,26 @@ class GenreList(APIView):
 
 class GenreDetail(APIView):
     def get_object(self, pk):
-        try:
-            return Genre.objects.get(pk=pk)
-        except Genre.DoesNotExist:
-            return Http404
+        return get_object_or_404(Genre, id=pk)
 
     def get(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, pk):
         genre = self.get_object(pk)
@@ -80,6 +85,9 @@ class ActorDetail(
 
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
