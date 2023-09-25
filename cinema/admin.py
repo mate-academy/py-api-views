2c20d755822c59af25d1ce45879e5a8255426665
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-09-25 18:01:55 +0300

    Solution

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-
-from cinema.models import Movie
+from cinema.models import Movie, Genre, Actor, CinemaHall
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        "title",
+        "description",
+        "duration"
+    ]
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name"]
+
+
+@admin.register(CinemaHall)
+class CinemaHallAdmin(admin.ModelAdmin):
+    list_display = ["name", "rows", "seats_in_row"]
+
+
+admin.site.register(Genre)
