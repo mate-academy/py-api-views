7055be11df49b16e988c9bc017a6d727512c6aea
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-27 22:05:35 +0300

    Create MovieViewSet using viewsets.ModelViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -156,3 +156,7 @@ class CinemaHallViewSet(
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
 
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
