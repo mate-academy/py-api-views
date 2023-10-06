ef96458afd613d969fae2602a412f2477d02f28c
Mazur Mariya <mazurinka29@gmail.com>
2023-10-06 12:06:32 +0300

    Add argument raise_exception to is_valid method.

--- cinema/views.py
+++ cinema/views.py
@@ -39,14 +39,14 @@ class GenreDetail(APIView):
     def put(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data)
-        serializer.is_valid()
+        serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
     def patch(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre, data=request.data, partial=True)
-        serializer.is_valid()
+        serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
