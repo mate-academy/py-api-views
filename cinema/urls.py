01c8f7d8c8a7dbad7aa51e0dfda3931dddfc248b
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-08 22:07:11 +0300

    add url resolution for cinema_halls

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from cinema.views import MovieViewSet, GenreList, GenreDetail, ActorList, ActorDetail
+from cinema.views import (
+    MovieViewSet, GenreList, GenreDetail,
+    ActorList, ActorDetail, CinemaHallViewSet
+)
+
+cinema_hall_list = CinemaHallViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+})
+
+cinema_hall_detail = CinemaHallViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
 
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet, basename="movies")
@@ -11,7 +26,9 @@ urlpatterns = [
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
-    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail")
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
 ]
 
 app_name = "cinema"
