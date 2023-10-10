21686a5d01e84a60a435c482b4efad1ae249495a
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-10-10 16:41:30 +0300

    update serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,7 +6,7 @@ from cinema.models import Movie, Actor, Genre, CinemaHall
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = "id", "title", "description", "duration"
+        fields = ["id", "title", "description", "duration"]
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
