dc65f32233573f6d4a0b6a210a25b494f9ae7e1e
Vlad Kleshko <vkoesko@gmail.com>
2023-10-12 19:01:43 +0300

    Implemented GenreSerializer with using ModelSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -45,19 +45,10 @@ class ActorSerializer(serializers.Serializer):
         return instance
 
 
-class GenreSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(unique=True, max_length=255)
-
-    def create(self, validated_data):
-        return Genre.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-
-        instance.save()
-
-        return instance
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
 
 
 class CinemaHallSerializer(serializers.Serializer):
