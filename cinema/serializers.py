220062504357caf14b64ced33839c8fa3f14a012
Vlad Kleshko <vkoesko@gmail.com>
2023-10-12 18:58:55 +0300

    Added unique=True to the GenreSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -47,7 +47,7 @@ class ActorSerializer(serializers.Serializer):
 
 class GenreSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
+    name = serializers.CharField(unique=True, max_length=255)
 
     def create(self, validated_data):
         return Genre.objects.create(**validated_data)
