f359312a1e607186696d128eb98bc6c954ee26c5
Munkoor <eee1lvlboss@gmail.com>
2023-10-14 16:18:05 +0300

    fixed

--- cinema/urls.py
+++ cinema/urls.py
@@ -16,13 +16,13 @@ router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
-
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
 
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+
+    path("", include(router.urls)),
 ]
 
 app_name = "cinema"
