63988970cfc5d75239154324121e1781bbf87be3
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-11 10:21:36 +0200

    Added  variable id

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -4,6 +4,7 @@ from cinema.models import Movie, Actor, Genre, CinemaHall
 
 
 class ActorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=255)
     last_name = serializers.CharField(max_length=255)
 
@@ -11,7 +12,9 @@ class ActorSerializer(serializers.Serializer):
         return Actor.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.first_name = validated_data.get(
+            "first_name", instance.first_name
+        )
         instance.last_name = validated_data.get(
             "last_name", instance.last_name
         )
@@ -20,6 +23,7 @@ class ActorSerializer(serializers.Serializer):
 
 
 class GenreSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
 
     def create(self, validated_data):
@@ -31,7 +35,8 @@ class GenreSerializer(serializers.Serializer):
         return instance
 
 
-class CinemaHallSerializers(serializers.Serializer):
+class CinemaHallSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
     rows = serializers.IntegerField()
     seats_in_row = serializers.IntegerField()
@@ -40,9 +45,15 @@ class CinemaHallSerializers(serializers.Serializer):
         return CinemaHall.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.rows = validated_data.get("rows", instance.rows)
-        instance.seats_in_row = validated_data.get("seats_in_row", instance.seats_in_row)
+        instance.name = validated_data.get(
+            "name", instance.name
+        )
+        instance.rows = validated_data.get(
+            "rows", instance.rows
+        )
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row", instance.seats_in_row
+        )
         instance.save()
         return instance
 
@@ -52,7 +63,6 @@ class MovieSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
     duration = serializers.IntegerField()
-
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
