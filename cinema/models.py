d1e0da5399439ee697e818bf2a1199c0b9c54a11
Hlova Oleh <hlovaoleh0@gmail.com>
2023-11-19 03:37:09 +0200

    implemented py-api-views task

--- cinema/models.py
+++ cinema/models.py
@@ -1,9 +1,38 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(
+        to=Actor,
+        related_name="movies"
+    )
+    genres = models.ManyToManyField(
+        to=Genre,
+        related_name="movies"
+    )
     duration = models.IntegerField()
 
     def __str__(self):
