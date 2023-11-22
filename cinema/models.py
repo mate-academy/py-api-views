3f03d054d79170bcb42dc76d2995fd0cf24d7a89
serhii_loboda <serhiy.loboda@gmail.com>
2023-11-22 11:21:11 +0200

    Solution: API Views.

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,44 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cinema_hall"
+        verbose_name_plural = "cinema_halls"
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    genres = models.ManyToManyField(
+        to=Genre, related_name="movies"
+    )
+    actors = models.ManyToManyField(
+        to=Actor, related_name="movies"
+    )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
