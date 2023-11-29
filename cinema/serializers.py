418cd07d4f1c1bddd6a6aa024797a9a7aaf8897b
Ihor Prochnickiy <2prochigor@gmail.com>
2023-11-29 13:58:42 +0200

    Fix movie serializator create method

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -16,7 +16,12 @@ class MovieSerializer(serializers.Serializer):
     )
 
     def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+        actors = validated_data.pop("actors")
+        genres = validated_data.pop("genres")
+        movie = Movie.objects.create(**validated_data)
+        movie.actors.set(actors)
+        movie.genres.set(genres)
+        return movie
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
