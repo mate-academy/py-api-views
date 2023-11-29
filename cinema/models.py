cee14c15a69d14f9ffdb9472b45d47fbeda709d7
OlehShumov <shumovoleh@ukr.net>
2023-11-29 13:51:48 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Actor(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
 
     def __str__(self):
         return (f"{self.first_name}, "
@@ -11,14 +11,14 @@ class Actor(models.Model):
 
 
 class Genre(models.Model):
-    name = models.CharField(unique=True, max_length=255)
+    name = models.CharField(unique=True, max_length=63)
 
     def __str__(self):
         return self.name
 
 
 class CinemaHall(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=63)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
