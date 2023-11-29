17d59717e1d15acaf48a847c52f1fbbe00004c9c
Denys <den.prislipskyi@gmail.com>
2023-11-29 21:04:32 +0100

    class-based views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,46 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (GenreList,
+                          GenreDetail,
+                          ActorList,
+                          ActorDetail,
+                          CinemaHallViewSet,
+                          MovieViewSet)
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+
+cinamehall_list = CinemaHallViewSet.as_view(
+    actions={"get": "list", "post": "create"}
+)
+cinamehall_detail = CinemaHallViewSet.as_view(
+    actions={"get": "retrieve",
+             "put": "update",
+             "path": "partial_update",
+             "delete": "destroy"}
+)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("movies/", include(router.urls)),
+    path("genres/",
+         GenreList.as_view(),
+         name="genre-list"),
+    path("genres/<int:pk>/",
+         GenreDetail.as_view(),
+         name="genre-detail"),
+    path("actors/",
+         ActorList.as_view(),
+         name="actor-list"),
+    path("actors/<int:pk>/",
+         ActorDetail.as_view(),
+         name="actor-detail"),
+    path("cinemahall/",
+         cinamehall_list,
+         name="cinemahall-list"),
+    path("cinemahall/<int:pk>/",
+         cinamehall_detail,
+         name="cinemahall-detail"),
 ]
 
 app_name = "cinema"
