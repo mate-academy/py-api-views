7ec686bedbf1ca8f3621a091c94931594bb8031a
wonsky <pidborskii17@gmail.com>
2023-11-30 01:50:13 +0200

    Implemented detail view for Actor

--- cinema/views.py
+++ cinema/views.py
@@ -81,3 +81,19 @@ class ActorList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Generic
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
+
+class ActorDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, *kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
