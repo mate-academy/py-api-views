b781e8038947d17785c44802c46cbe4096388b15
wonsky <pidborskii17@gmail.com>
2023-11-30 01:43:00 +0200

    Hotfix

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from cinema.views import GenreList, GenreDetail
 
 urlpatterns = [
-    path("movies/", GenreList.as_view(), name="movie-list"),
-    path("movies/<int:pk>/", GenreDetail.as_view(), name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="movie-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="movie-detail"),
 ]
 
 app_name = "cinema"
