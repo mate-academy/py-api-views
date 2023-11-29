5fb9bdb5a36bcd25f8b99c70bda9bc885929526b
Eugene Rabenko <qwer09846@gmail.com>
2023-11-29 16:49:02 +0200

    Corrected

--- cinema/views.py
+++ cinema/views.py
@@ -28,7 +28,7 @@ class GenreList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(
-                serializer.data, status=status.HTTP_200_OK
+                serializer.data, status=status.HTTP_201_CREATED
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
