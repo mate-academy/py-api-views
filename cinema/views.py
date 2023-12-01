e95ea3bde597be890ddd46c176edc4f69a1cc4a1
Serhii <martinenko.sergiy@gmail.com>
2023-12-01 16:37:14 +0200

    fix

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,3 @@
-from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status, generics, mixins, viewsets
 
@@ -28,15 +27,15 @@ class GenreList(APIView):
 
 class GenreDetail(APIView):
     def get_object(self, pk):
-        return get_object_or_404(Genre, pk)
+        return get_object_or_404(Genre, pk=pk)
 
     def get(self, request, pk):
-        genre = self.get_object(pk)
+        genre = self.get_object(pk=pk)
         serializer = GenreSerializer(genre)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
-        genre = self.get_object(pk)
+        genre = self.get_object(pk=pk)
         serializer = GenreSerializer(genre, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -44,7 +43,7 @@ class GenreDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, pk):
-        genre = self.get_object(pk)
+        genre = self.get_object(pk=pk)
         serializer = GenreSerializer(genre, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
@@ -52,7 +51,7 @@ class GenreDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        genre = self.get_object(pk)
+        genre = self.get_object(pk=pk)
         genre.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
