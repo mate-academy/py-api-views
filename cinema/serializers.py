72615924a9f765865a01095d34b47ba0c17cd574
Oleksandr Pupchenko <oleksandr.pupchenko@gmail.com>
2023-12-10 23:37:07 +0100

    fix

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,33 +1,38 @@
 from rest_framework import serializers
 
+from cinema.models import Movie
 from cinema.models import Movie, Genre, Actor, CinemaHall
 
 
 class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
+    duration = serializers.IntegerField(min_value=1)
     actors = serializers.PrimaryKeyRelatedField(
         required=False, queryset=Actor.objects.all(), many=True
     )
     genres = serializers.PrimaryKeyRelatedField(
         required=False, queryset=Genre.objects.all(), many=True
     )
-    duration = serializers.IntegerField(min_value=1)
 
     def create(self, validated_data):
         actors_data = validated_data.pop("actors", [])
         genres_data = validated_data.pop("genres", [])
+
+        # Створення об'єкта Movie із значеннями actors та genres
         movie = Movie.objects.create(**validated_data)
         movie.actors.set(actors_data)
         movie.genres.set(genres_data)
+
         return movie
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get(
-            "description", instance.description
-        )
+        instance.description = validated_data.get("description", instance.description)
         instance.duration = validated_data.get("duration", instance.duration)
+
+        instance.save()
 
         actors_data = validated_data.get("actors")
         if actors_data is not None:
@@ -37,7 +42,6 @@ class MovieSerializer(serializers.Serializer):
         if genres_data is not None:
             instance.genres.set(genres_data)
 
-        instance.save()
         return instance
 
 
