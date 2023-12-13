34e67482b416ffaa4e1679d5f0c36419eecd82d4
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-13 15:58:40 +0200

    create migrations, change related_name in models

--- cinema/models.py
+++ cinema/models.py
@@ -28,8 +28,8 @@ class CinemaHall(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor, related_name="movies")
-    genres = models.ManyToManyField(Genre, related_name="movies")
+    actors = models.ManyToManyField(Actor, related_name="movie_actors")
+    genres = models.ManyToManyField(Genre, related_name="movie_genres")
     duration = models.IntegerField()
 
     def __str__(self):
