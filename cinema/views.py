2c6ecdfa6dd0caf267eb639a892bb16c079f00d9
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2024-01-02 11:52:45 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,3 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, generics, mixins, viewsets
 
@@ -15,7 +14,7 @@ from cinema.serializers import (
 
 
 class GenreList(APIView):
-    def get(self):
+    def get(self, request):
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
