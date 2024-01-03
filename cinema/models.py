0febaf9f9367cdf37e804828a788ed4c1744cf9d
Denys Raboshchuk <kontroler444@gmail.com>
2024-01-03 18:05:15 +0200

    Implement models: Actor, Genre, CinemaHall. Update model Movie. Make serializers and views for all of this models

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,37 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    actors = models.ManyToManyField(
+        Actor,
+        related_name="movies",
+        blank=True,
+        null=True
+    )
+    genres = models.ManyToManyField(
+        Genre,
+        related_name="movies",
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=64)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
