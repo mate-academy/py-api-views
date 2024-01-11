af496585f50443aad91ad8a23360882f44d9edb0
Max Lapchuk <makslapchuk@gmail.com>
2024-01-11 22:18:48 +0200

    Change urlpatterns in urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-list"),
     path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-detail"),
-    path("", include(router.urls))
-]
+] + router.urls
 
 app_name = "cinema"
