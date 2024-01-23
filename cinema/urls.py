c10c39508e020838c58c0259dab572162222f0b6
Soloveyk1n <swain.volt@gmail.com>
2024-01-23 18:25:33 +0200

    test_sol2

--- cinema/urls.py
+++ cinema/urls.py
@@ -12,26 +12,26 @@ from rest_framework import routers
 
 
 cinema_list = CinemaHallViewSet.as_view(actions={
-            "get": "list",
-            "post": "create",
-        })
+    "get": "list",
+    "post": "create",
+})
 cinema_detail = CinemaHallViewSet.as_view(actions={
-            "get": "retrieve",
-            "put": "update",
-            "patch": "partial_update",
-            "delete": "destroy",
-        })
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
     path("cinema-halls/", cinema_list, name="cinema-halls-list"),
-    path("cinema-halls/<pk>/", cinema_detail, name="cinema-halls-detail"),
+    path("cinema-halls/<int:pk>/", cinema_detail, name="cinema-halls-detail"),
     path("genres/", GenreList.as_view(), name="genre-list"),
-    path("genres/<pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
-    path("actors/<pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
 ]
 
 app_name = "cinema"
