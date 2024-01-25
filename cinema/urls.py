c4204b70d045cc4f55ba1615540c7b3fb63754f9
3lobites <3lobites@gmail.com>
2024-01-25 15:48:03 +0200

    Add CinemaHall model use an GenericViewSet. Edited: cinema/views (CinemaHallViewSet), cinema/serializers (CinemaHallSerializer), cinema\urls(added path cinema_hall)

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail, GenreList, GenreDetail, ActorList, ActorDetail
+from cinema.views import movie_list, movie_detail, GenreList, GenreDetail, ActorList, ActorDetail, CinemaHallViewSet
+
+cinema_hall_list = CinemaHallViewSet.as_view({"get": "list", "post": "create"})
+cinema_hall_detail = CinemaHallViewSet.as_view({"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy", })
+
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
@@ -9,7 +13,8 @@ urlpatterns = [
     path("genre/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actor/", ActorList.as_view(), name="actor-list"),  # GenericAPIView
     path("actor/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-
+    path("cinema_hall/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema_hall/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
 ]
 
 app_name = "cinema"
