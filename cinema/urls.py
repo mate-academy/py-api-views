10f79f623121213d26e4a7263f5da907130de5d6
Ivan Romanov <i.romanov.dn@gmail.com>
2024-02-06 00:06:08 +0200

    fix cinema/urls.py solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -8,11 +8,11 @@ from cinema.views import (MovieViewSet,
                           GenreList,
                           GenreDetail)
 
-cinema_hall_list = CinemaHallViewSet.as_view(
-    {"get": "list", "post": "create"}
-)
+cinema_hall_list = CinemaHallViewSet.as_view(actions={
+    "get": "list", "post": "create"
+})
 
-cinema_hall_detail = CinemaHallViewSet.as_view({
+cinema_hall_detail = CinemaHallViewSet.as_view(actions={
     "get": "retrieve",
     "put": "update",
     "patch": "partial_update",
@@ -23,13 +23,13 @@ router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("genre/", GenreList.as_view(), name="genre-list"),
-    path("genre/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("actor/", ActorList.as_view(), name="actor-list"),
-    path("actor/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema-hall/", cinema_hall_list, name="cinema-hall-list"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
-        "cinema-hall/<int:pk>",
+        "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-hall-detail"
     ),
