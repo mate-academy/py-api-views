54a8685ed21a153a7b5cb84c3676ba3aceb473c8
Ruslan Yanyshyn <ruslan.yanyshyn@yahoo.com>
2024-02-14 09:57:44 -0600

    Mix of rest API views implementation

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,33 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f" {self.first_name} {self.last_name}"
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return f" {self.name}"
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
 
     def __str__(self):
-        return self.title
+        return f" {self.title}"
