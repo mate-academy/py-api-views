2ab0b370fd2ff89e5723739412fce84d7d743bee
Vlad Krit <kritvladislav98@gmail.com>
2024-03-03 02:00:13 +0200

    Solution py-api-views

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Actor, Genre, CinemaHall
 
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
+    actors = serializers.PrimaryKeyRelatedField(queryset=Actor.objects.all(), many=True)
+    genres = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), many=True)
     duration = serializers.IntegerField()
 
     def create(self, validated_data):
@@ -17,8 +19,52 @@ class MovieSerializer(serializers.Serializer):
         instance.description = validated_data.get(
             "description", instance.description
         )
+        instance.actors = validated_data.get("actors", instance.actors)
+        instance.genres = validated_data.get("genres", instance.genres)
         instance.duration = validated_data.get("duration", instance.duration)
 
         instance.save()
 
+        return instance
+
+
+class ActorSerializer(serializers.Serializer):
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.save()
+        return instance
+
+
+class GenreSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Genre.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.save()
+        return instance
+
+
+class CinemaHallSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    rows = serializers.IntegerField()
+    seats_in_row = serializers.IntegerField()
+
+    def create(self, validate_data):
+        return CinemaHall.objects.create(**validate_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", insance.name)
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.seats_in_row = validated_data.get("seats_in_row", instance.seats_in_row)
+        instance.save()
         return instance
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,45 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import(
+    GenreList,
+    GenreDetail,
+    ActorList,
+    ActorDetail,
+    CinemaHallViewSet,
+    MovieViewSet
+)
+
+router = routers.DefaultRouter()
+
+router.register("movies", MovieViewSet)
+
+cinemahall_list = CinemaHallViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create",
+    }
+)
+cinemahall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
+)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genre_list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre_detail"),
+
+    path("actors/", ActorList.as_view(), name="actor_list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor_detail"),
+
+    path("cinemahalls/", cinemahall_list, name="cinemahall_list"),
+    path("cinemahalls/<int:pk>/", cinemahall_detail, name="cinemahall_detail"),
+
+    path("", include(router.urls))
 ]
 
 app_name = "cinema"
