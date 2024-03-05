36994c9f72e03c6dbd85acdd1a3a8dda32a91949
Ruslan <rusipbox@gmail.com>
2024-03-05 03:54:28 +0200

    (py-api-views)

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,34 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=155)
+    last_name = models.CharField(max_length=155)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} ({self.rows} rows of {self.seats_in_row} seats)"
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
