1c6d45c107295e2263b3569cbf0e6c99cf7caf66
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-11 19:00:44 +0200

    Add actors & genres manytomany fields to Movie model

--- cinema/models.py
+++ cinema/models.py
@@ -29,6 +29,8 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    actors = models.ManyToManyField(to=Actor, related_name="movies")
+    genres = models.ManyToManyField(to=Genre, related_name="movies")
 
     def __str__(self):
         return self.title
