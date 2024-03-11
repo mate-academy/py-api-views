b5ae34ec9f5b50d3d582e73805a23b716081002d
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-11 22:04:00 +0200

    Add ActorLisView to views.py

--- cinema/views.py
+++ cinema/views.py
@@ -1,12 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, mixins
 
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
-from cinema.models import Movie, Genre
-from cinema.serializers import MovieSerializer, GenreSerializer
+from cinema.models import Movie, Genre, Actor
+from cinema.serializers import MovieSerializer, GenreSerializer, ActorSerializer
 
 
 @api_view(["GET", "POST"])
@@ -91,3 +91,18 @@ class GenreDetailView(APIView):
         genre = self.get_object(pk)
         genre.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ActorListView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def get(self, request, *args, **kwargs) -> Response:
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs) -> Response:
+        return self.create(request, *args, **kwargs)
