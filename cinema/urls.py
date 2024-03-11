4fca1d44a5681dc3443b5e13bb9905e256f31f3e
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-11 23:11:50 +0200

    Changes for flake8

--- cinema/urls.py
+++ cinema/urls.py
@@ -37,8 +37,11 @@ urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list_view, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail_view, name="cinema-hall-detail"),
-
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail_view,
+        name="cinema-hall-detail"
+    ),
 ]
 
 app_name = "cinema"
