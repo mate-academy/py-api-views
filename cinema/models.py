dee7e1a5fb8e35c2f202c2c329ba902a7ba0c07c
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-14 19:17:42 +0200

    add str mtd for models

--- cinema/models.py
+++ cinema/models.py
@@ -5,15 +5,24 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=30)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 class Movie(models.Model):
