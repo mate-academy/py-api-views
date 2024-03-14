9029baa3d4809df5aba97797306d16a50f5db98d
AntonSkyrda <mr.skyrda@gmail.com>
2024-03-14 18:24:23 +0200

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -6,12 +6,21 @@ from cinema.views import (
     GenreDetail,
     ActorList,
     ActorDetail,
-    CinemaHallList,
-    CinemaHallDetail,
+    CinemaHallViewSet,
     MovieViewSet
 )
 
 app_name = "cinema"
+
+cinema_hall_list = CinemaHallViewSet.as_view(actions={"get": "list", "post": "create"})
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
+)
 
 router = routers.DefaultRouter()
 
@@ -22,10 +31,10 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema-halls/", CinemaHallList.as_view(), name="cinema-hall-list"),
+    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
         "cinema-halls/<int:pk>/",
-        CinemaHallDetail.as_view(),
+       cinema_hall_detail,
         name="cinema-hall-detail"
     ),
     path("", include(router.urls))
