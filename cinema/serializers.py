d3d68318a1b74c4e20d62b192a83e4e4d7708bd7
Tymofii Skopenko <youmustdeth@gmail.com>
2024-03-14 19:22:34 +0200

    add new line at the classes according to the pep8

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -25,18 +25,21 @@ class MovieSerializer(serializers.Serializer):
 
 
 class ActorSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Actor
         fields = "__all__"
 
 
 class CinemaHallSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = CinemaHall
         fields = "__all__"
 
 
 class GenreSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Genre
         fields = "__all__"
