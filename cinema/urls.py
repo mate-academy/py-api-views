bc88410b8d2af7a2449a5d7780557260d8c720cc
barabarinov <barabarinov@gmail.com>
2024-03-14 16:54:12 +0200

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,44 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    MovieViewSet,
+    GenreList,
+    GenreDetail,
+    ActorList,
+    ActorDetail,
+    CinemaHallViewSet,
+)
 
-urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
-]
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+
+cinema_hall_list = CinemaHallViewSet.as_view(actions={"get": "list", "post": "create"})
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
+)
 
 app_name = "cinema"
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("genres/", GenreList.as_view(), name="genres-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actors-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actors-detail"),
+    path(
+        "cinemahalls/",
+        cinema_hall_list,
+        name="cinema-hall-list",
+    ),
+    path(
+        "cinemahalls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail",
+    ),
+]
