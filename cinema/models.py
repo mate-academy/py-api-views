17f852f2d6fd3e78caf98c52586dd684d7cb5f28
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-14 17:51:04 +0200

    create new models: Actor gener adn CinemaHall (+ migreate to db)

--- cinema/models.py
+++ cinema/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=30)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
     duration = models.IntegerField()
 
     def __str__(self):
