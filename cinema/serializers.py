8a8cfb930b0b1254d04af5f7109b2951d68525c5
Artem Hryshko <artemggg2222@gmail.com>
2024-03-17 13:31:24 +0200

    Solution3

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -4,24 +4,28 @@ from .models import Actor, Genre, CinemaHall, Movie
 
 
 class ActorSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Actor
         fields = ["id", "first_name", "last_name"]
 
 
 class GenreSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Genre
         fields = ["id", "name"]
 
 
 class CinemaHallSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = CinemaHall
         fields = ["id", "name", "rows", "seats_in_row"]
 
 
 class MovieSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Movie
         fields = ["id", "title", "description", "duration"]
